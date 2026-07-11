39cea797601b9291b3eabef1f097a5d9c3848e46
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-07-11 02:55:42 -0700

    Use Google for now until changing search engine feature is added

--- src/search.js
+++ src/search.js
@@ -1,9 +1,5 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `Count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+import engines from "./engines.json";
+
+export function search(query) {
+  window.location.href = `${engines[0].prefix}${query}`;
 }
