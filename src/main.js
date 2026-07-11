5b4e9e41a6cb7818224cf6cf65303ea557254940
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-07-11 03:13:56 -0700

    Add basic search engine dropdown

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import javascriptLogo from "./assets/javascript.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import tab from "./tab.module.css";
+import engines from "./engines.json";
 import { search } from "./search.js";
 
 document.querySelector("#app").innerHTML = `
@@ -11,6 +12,14 @@ document.querySelector("#app").innerHTML = `
     <form id="search">
         <input type="text" id="searchbar" placeholder="Search the web"></input>
     </form>
+    <details class="${tab.dropdown}">
+        <summary>Search Engines</summary>
+        <ul>
+            ${engines.map((engine) => {
+              return `<li><a href="#">${engine.name}</a></li>`;
+            })}
+        </ul>
+    </details
 </section>
 `;
 
