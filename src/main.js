4f205ad93b0bb70e23688cb254e0fff2c9e2b3e2
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-07-11 02:42:15 -0700

    Add search bar and tab css module

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import "./style.css";
 import javascriptLogo from "./assets/javascript.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
+import tab from "./tab.module.css";
 import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = ``;
+document.querySelector("#app").innerHTML = `
+<section class="${tab.main}">
+    <h1 class="${tab.title}">SuperiorTab</h1>
+    <form>
+        <input type="text" placeholder="Search the web"></input>
+    </form>
+</section>
+`;
 
 setupCounter(document.querySelector("#counter"));
