95f7db27cdb5e84f66c03de0b4b79bef372bafb6
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-07-11 02:56:51 -0700

    Use search function on form submit

--- src/main.js
+++ src/main.js
@@ -3,15 +3,19 @@ import javascriptLogo from "./assets/javascript.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import tab from "./tab.module.css";
-import { setupCounter } from "./counter.js";
+import { search } from "./search.js";
 
 document.querySelector("#app").innerHTML = `
 <section class="${tab.main}">
     <h1 class="${tab.title}">SuperiorTab</h1>
-    <form>
-        <input type="text" placeholder="Search the web"></input>
+    <form id="search">
+        <input type="text" id="searchbar" placeholder="Search the web"></input>
     </form>
 </section>
 `;
 
-setupCounter(document.querySelector("#counter"));
+document.querySelector("#search").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  search(document.querySelector("#searchbar").value.trim());
+});
