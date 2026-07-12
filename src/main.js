1d21da03f21e65390f5fa75f6e779a3c52e85abe
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-07-11 18:13:12 -0700

    Add dropdown search engine select function

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import heroImg from "./assets/hero.png";
 import tab from "./tab.module.css";
 import engines from "./engines.json";
 
+let selectedEngine = engines[0];
+
 document.querySelector("#app").innerHTML = `
 <section class="${tab.main}">
     <h1 class="${tab.title}">SuperiorTab</h1>
@@ -12,8 +14,8 @@ document.querySelector("#app").innerHTML = `
         <input type="text" id="searchbar" placeholder="Search the web"></input>
     </form>
     <details class="${tab.dropdown}">
-        <summary>Search Engines</summary>
-        <ul>
+        <summary>${selectedEngine.name}</summary>
+        <ul id="items">
             ${engines
               .map((engine) => {
                 return `<li>${engine.name}</li>`;
@@ -24,7 +26,16 @@ document.querySelector("#app").innerHTML = `
 </section>
 `;
 
+const items = document.querySelectorAll("#items li");
+
+items.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    selectedEngine = engines[index];
+    document.querySelector("summary").textContent = selectedEngine.name;
+  });
+});
+
 document.querySelector("#search").addEventListener("submit", (event) => {
   event.preventDefault();
-  window.location.href = `${engines[0].prefix}${document.querySelector("#searchbar").value.trim()}`;
+  window.location.href = `${selectedEngine.prefix}${document.querySelector("#searchbar").value.trim()}`;
 });
